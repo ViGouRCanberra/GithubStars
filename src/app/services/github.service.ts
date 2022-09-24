e90e078102d5ee7320f31c01a969e78481604bfe
Le-Roux Enslin <chocobo.leroux@gmail.com>
2022-09-24 17:03:55 +0200

    Use date for 30 days ago on github endpoint query instead of hardcoded 2017 date

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,25 +1,37 @@
 import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { RepoData } from '../structures/repoData';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
-  // TODO Pull today's date and use it instead of 2017-10-22
-  //private githubEndpoint = 'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc';
-  private githubEndpoint = '/assets/github-example.json';
+  private githubEndpoint = 'https://api.github.com/search/repositories?q=created:>';
+  private githubEndpointParams = '&sort=stars&order=desc';
 
   constructor(
     private http: HttpClient
-  ) { }
+  ) {}
 
   getGithubRepoInfo(): Observable<RepoData> {
     //TODO Add ability to specify page
 
-    return this.http.get<RepoData>(this.githubEndpoint, {
+    return this.http.get<RepoData>(this.buildGithubEndpoint(), {
       'responseType': 'json'
     });
+  }
+
+  buildGithubEndpoint(): string {
+    return `${this.githubEndpoint}${this.getDateThirtyDaysAgo()}${this.githubEndpointParams}`;
+  }
+
+  getDateThirtyDaysAgo(): string {
+    let thirtyDaysAgo = new Date();
+
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    return formatDate(thirtyDaysAgo, 'y-MM-d', 'en');
   }
 }
